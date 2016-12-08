df5df55bad38e22d7336235aecf110c643a57e55
Dilip Kumar <dilip.programmer@gmail.com>
2016-12-08 21:19:10 +0530

    New fragment

--- app/src/main/java/com/playground/dilip/androidplayground/fragments/RssFragment.java
+++ app/src/main/java/com/playground/dilip/androidplayground/fragments/RssFragment.java
@@ -5,7 +5,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.playground.dilip.androidplayground.R;
 
@@ -13,14 +12,11 @@ import com.playground.dilip.androidplayground.R;
  * Created by Dilip on 12/8/2016.
  */
 
-public class FirstFragment extends Fragment implements View.OnClickListener {
+public class RssFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_first, container, false);
-
-        Button buttonRssFeed = (Button) view.findViewById(R.id.button_rss);
-        buttonRssFeed.setOnClickListener(this);
+        View view = inflater.inflate(R.layout.fragment_rss, container, false);
 
         return view;
     }
@@ -28,9 +24,5 @@ public class FirstFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View view) {
 
-        switch (view.getId()) {
-            case R.id.button_rss:
-                break;
-        }
     }
 }
